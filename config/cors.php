a0371b9f26ca175610d352f24576f4e234f6d40b
muhammaddonifirmansyah43-dotcom <muhammaddonifirmansyah43@gmail.com>
2026-06-09 16:12:38 +0700

    add cors config for vercel frontend

---
+++ config/cors.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://uas-bismillah-magangku-6xdg.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.vercel\.app$/',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
